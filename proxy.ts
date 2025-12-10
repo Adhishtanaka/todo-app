aff5f32ec4cd2901b61d0d503c9e5567e810d0a5
Adhishtanaka Kulasooriya <kulasoooriyaa@gmail.com>
2025-12-10 23:47:24 +0530

    also change middlware name to proxy so new version use that name

--- proxy.ts
+++ proxy.ts
@@ -11,7 +11,7 @@ const apiPaths = [
   '/api/todo',
 ];
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const isPathProtected = protectedPaths.some((path) => 
